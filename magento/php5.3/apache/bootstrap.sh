bcfed9be8ae8313301fc33eb430a8c8accd3b870
Hadrien <hadrien@occitech.fr>
2014-10-03 15:54:23 +0200

    Fully silent entrypoints

--- magento/php5.3/apache/bootstrap.sh
+++ magento/php5.3/apache/bootstrap.sh
@@ -7,11 +7,11 @@ if test -f $localXml; then
   rm $localXml
 fi
 
-/usr/bin/n98-magerun --root-dir=${magentoRootDir} local-config:generate ${MPSEC_DB_1_PORT_3306_TCP_ADDR} root root mpsec files admin >/dev/null
+/usr/bin/n98-magerun --root-dir=${magentoRootDir} local-config:generate ${MPSEC_DB_1_PORT_3306_TCP_ADDR} root root mpsec files admin >/dev/null 2>&1
 
 portLinkedTo80=$(docker ps -a | grep mpsec_web_1 | grep -o -P "[0-9]+\->80" | tr "\->" "\n" | head -n 1)
 baseUrl="http://127.0.0.1:${portLinkedTo80}/"
-/usr/bin/n98-magerun --root-dir=${magentoRootDir} config:set web/unsecure/base_url ${baseUrl} >/dev/null
-/usr/bin/n98-magerun --root-dir=${magentoRootDir} config:set web/secure/base_url ${baseUrl} >/dev/null
+/usr/bin/n98-magerun --root-dir=${magentoRootDir} config:set web/unsecure/base_url ${baseUrl} >/dev/null 2>&1
+/usr/bin/n98-magerun --root-dir=${magentoRootDir} config:set web/secure/base_url ${baseUrl} >/dev/null 2>&1
 
 sh /init.sh "$@"
--- php/5.4/apache/init.sh
+++ php/5.4/apache/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mailcatcher --http-ip 0.0.0.0
+mailcatcher --http-ip 0.0.0.0 >/dev/null 2>&1
 
-exec $@
+exec "$@"
