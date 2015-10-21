f3017e4ded1770205db5dd2df85a169d2bf8cd1f
Pierre Martin <pierre@occitech.fr>
2015-10-21 10:08:57 +0200

    Add several steps to the audit

--- magento/audit/do-audit.sh
+++ magento/audit/do-audit.sh
@@ -12,8 +12,25 @@ if [ ! -d "$MAGE_ORIG_SRC" ]; then
   download-magento $MAGE_VERSION $MAGE_ORIG_SRC
 fi
 
-echo "2 - Core hacks analysis"
-n98-magerun mpmd:corehacks $MAGE_ORIG_SRC $AUDIT_DEST/corehacks.html
+n98-magerun --skip-root-check cache:c
 
-echo "3 - Core hacks analysis"
-n98-magerun mpmd:codepooloverrides $AUDIT_DEST/overrides.html
+echo "2 - Core hacks analysis"
+n98-magerun --skip-root-check mpmd:corehacks $MAGE_ORIG_SRC $AUDIT_DEST/corehacks.html
+
+echo "3 - Local overrides analysis"
+n98-magerun --skip-root-check mpmd:codepooloverrides $AUDIT_DEST/overrides.html
+
+echo "4 - Extensions analysis"
+n98-magerun --skip-root-check dev:module:list > $AUDIT_DEST/extensions.txt
+
+echo "5 - Rewrites & conflicts analysis"
+echo "All Rewrites" > $AUDIT_DEST/rewrites.txt
+n98-magerun --skip-root-check dev:module:rewrite:list >> $AUDIT_DEST/rewrites.txt
+echo "Rewrite conflicts" >> $AUDIT_DEST/rewrites.txt
+n98-magerun --skip-root-check dev:module:rewrite:conflicts >> $AUDIT_DEST/rewrite-conflicts.txt
+
+echo "6 - Gather further statistics"
+echo "Magento system info:" > $AUDIT_DEST/stats.txt
+n98-magerun --skip-root-check sys:info >> $AUDIT_DEST/stats.txt
+echo "Reports count:" >> $AUDIT_DEST/stats.txt
+n98-magerun --skip-root-check dev:report:count >> $AUDIT_DEST/stats.txt
